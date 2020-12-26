cf7fab39d14d638ff52a338f33ac2bf77026635f
Midhilesh <72137078+Midhilesh-19@users.noreply.github.com>
2020-12-25 20:12:20 -0800

    Add files via upload

---
+++ videotomp3.py
@@ -0,0 +1,10 @@
+import os
+import moviepy.editor as mp #pip install moviepy
+
+path = input("Enter the path of file :- ") #file path location
+
+videoclip = mp.VideoFileClip(path)
+audioclip = videoclip.audio
+audioclip.write_audiofile("12.mp3".format(path))#by using this the file will be coverted at the location place of code folder
+               #or
+#audioclip.write_audiofile("{}"12.mp3".format(path))''(this line will locate the converted file in where the actual video folder )''
